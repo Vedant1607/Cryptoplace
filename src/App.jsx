a684659bf0727f7655dd73f83af8e9f884045a80
Vedant1607 <sinhavedant0705@gmail.com>
2025-08-22 00:33:07 +0530

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Navbar from "@/components/Navbar/Navbar.jsx";
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home/Home";
+import Coin from "@/pages/Coin/Coin";
 
 const App = () => {
   return (
     <div class="min-h-screen text-white bg-gradient-to-b from-[#0b004e] via-[#1d152f] to-[#002834]">
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
     </div>
   )
 }

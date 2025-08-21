7569b50a77eb71147ebed59a0fb499f27a0f9f0f
Vedant1607 <sinhavedant0705@gmail.com>
2025-08-21 22:15:45 +0530

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import Navbar from "@/components/Navbar/Navbar.jsx";
 
 const App = () => {
   return (
-    <div></div>
+    <div class="min-h-screen text-white bg-gradient-to-b from-[#0b004e] via-[#1d152f] to-[#002834]">
+      <Navbar/>
+    </div>
   )
 }
 
